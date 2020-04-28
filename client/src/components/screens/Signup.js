34aac69ca0efedd85de79cd5b5c9bdef3ee59dec
tehpunisher456 <55720542+tehpunisher456@users.noreply.github.com>
2020-04-28 03:38:32 -0700

    i give up sorry for not doing much.

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -7,11 +7,19 @@ const Signup = () => {
   const [password, setPassword] = useState("")
   const [email, setEmail] = useState("")
 const PostData = ()=> {
-  fetch("https://localhost:5000/signup", {
+  fetch("/signup", {
     method: "post",
     headers:{
       "Content-Type": "application/json"
-    }
+    },
+    body: JSON.stringify({
+      name:"",
+      password:"",
+      email: ""
+    })
+  }).then(res => res.json())
+  .then(data=>{
+    console.log(data)
   })
 }
 
@@ -22,7 +30,7 @@ const PostData = ()=> {
         <input type="text" placeholder="name" value = {name} onChange = {(e) => setName(e.target.value)}/>
         <input type="text" placeholder="email" value = {email} onChange = {(e) => setEmail(e.target.value)}/>
         <input type="text" placeholder="password" value = {password} onChange = {(e) => setPassword(e.target.value)}/>
-        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" onClick={()=>PostData()}>
           Sign Up
         </button>
         <h5>
