d4509c48bbb2fdf7ba300d5ffa8cfc092916cc91
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-05-02 12:38:42 -0700

    styling of checkout page

--- client/src/components/screens/Checkout.js
+++ client/src/components/screens/Checkout.js
@@ -10,11 +10,13 @@ import "react-toastify/dist/ReactToastify.css";
 toast.configure();
 
 function Checkout() {
-  const [product] = React.useState({
-    name: "dummy text",
-    price: 20.6,
-    description: "Cool car"
-  });
+   const [product] = React.useState({
+     name: "Otter-shaped Tape Dispenser",
+     price: 20.6,
+     description: "Cool car",
+     image: "https://images-na.ssl-images-amazon.com/images/I/71kieVNLOWL._AC_SL1001_.jpg",
+     description: "The dummy product is Otter tape dispenser"
+   });
 
   async function handleToken(token, addresses) {
     const response = await axios.post(
@@ -31,19 +33,45 @@ function Checkout() {
   }
 
   return (
-    <div className="container">
-      <div className="product">
-        <h1>{product.name}</h1>
-        <h3>On Sale · ${product.price}</h3>
+    <div>
+
+      <div className="row card home-card">
+  
+        {/* <h5 style={{ padding: "5px" }}>
+        <Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"}>{item.postedBy.userName}</Link> {item.postedBy._id == state._id
+          && <i className="material-icons" style={{
+            float: "right"
+          }}
+            onClick={() => deletePost(item._id)}
+          >delete</i>
+
+        }</h5> */}
+
+        <div className="col s12">
+        <h1 className="product-name">Otter-shaped Tape Dispenser</h1>
+          <div className="card-image">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71kieVNLOWL._AC_SL1001_.jpg" className="product-image" />
+          </div>
+        </div>
+
+        <div className="col s12 card-content">
+          <div className="product">
+           
+            <h3 className="product-price">On Sale · $12.00</h3>
+            <p>"The dummy product is Otter tape dispenser"</p>
+          </div>
+          <StripeCheckout
+            stripeKey="pk_test_4TbuO6qAW2XPuce1Q6ywrGP200NrDZ2233"
+            token={handleToken}
+            amount={product.price * 100}
+            name={product.name}
+            billingAddress
+            shippingAddress
+          />
+        </div>
       </div>
-      <StripeCheckout
-        stripeKey="pk_test_4TbuO6qAW2XPuce1Q6ywrGP200NrDZ2233"
-        token={handleToken}
-        amount={product.price * 100}
-        name={product.name}
-        billingAddress
-        shippingAddress
-      />
+
+
     </div>
   );
 }
